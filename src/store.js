0bcf96194d52c7e9ec42fbf9ddd199131ff6eae3
ManuelF <manuel.floriani1999@gmail.com>
2023-06-11 21:11:01 +0200

    Creazione store + componenti principali

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+    apiArchURL: "https://db.ygoprodeck.com/api/v7/archetypes.php",
+    archList: [],
+    archSelect: "",
+    cardsList: [],
+});
